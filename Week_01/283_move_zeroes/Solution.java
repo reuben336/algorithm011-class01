5202b7f18d97342579dfa82636445e3aa460d1dc
Reuben <39336599@qq.com>
2020-07-14 14:21:22 +0800

    Update 283 - move zeroes

--- Week_01/283_move_zeroes/Solution.java
+++ Week_01/283_move_zeroes/Solution.java
@@ -1,16 +1,36 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        if (nums == null || nums.length < 2) return;
 
-	int j = 0;
-	for (int i = 0; i < nums.length; ++i) {
-	    if (nums[i] != 0) {
-	        nums[j++] = nums[i];
-	    }
-	}
-	for (int i = j; i < nums.length; ++i) {
-	    nums[i] = 0;
-	}
+  public void moveZeroes1(int[] nums) {
+    if (nums == null || nums.length < 2) {
+      return;
     }
+
+    int j = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      if (nums[i] != 0) {
+        nums[j++] = nums[i];
+      }
+    }
+    for (int i = j; i < nums.length; ++i) {
+      nums[i] = 0;
+    }
+  }
+
+  public void moveZeroes2(int[] nums) {
+    if (nums == null || nums.length < 2) {
+      return;
+    }
+
+    int pos = 0;
+    for (int i = 0; i < nums.length; ++i) {
+      if (nums[i] != 0) {
+        if (i != pos) {
+          nums[pos] = nums[i];
+          nums[i] = 0;
+        }
+        pos++;
+      }
+    }
+  }
 }
 
